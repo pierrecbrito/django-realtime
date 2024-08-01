73803d84173524a558debd3e86fcd0b226452a03
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-08-01 18:03:30 -0300

    Tela de identidade

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "identidade.html")
+    if request.method == "GET":
+        return render(request, "identidade.html")
+    elif request.method == "POST":
+        nome = request.POST['nome']
+
+        if nome: 
+            ...
+        else:
+            return render(request, "identidade.html", {"mensagem": "Campo nome vazio ou inválido."})
