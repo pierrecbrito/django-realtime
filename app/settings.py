2331768874b833f56c36121a5f7ab1d5faa4b5eb
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-08-07 16:24:51 -0300

    Configuração do channels

--- app/settings.py
+++ app/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "app.asgi.application"
