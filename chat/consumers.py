afa63ba49e688bbe87837ac7a561219e72bb66ae
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-08-12 15:24:41 -0300

    Persistência de mensagens no banco

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,15 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Mensagem, Grupo
 
 #Nós temos muitas instâncias do ChatConsumer
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.sala = self.scope['url_route']['kwargs']['grupo']
         self.sala_grupo = f'chat_{self.sala}'
+        self.grupo = await self.get_grupo(self.sala)
 
         await self.channel_layer.group_add(
             self.sala_grupo,
@@ -26,6 +29,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         mensagem = text_data_json['mensagem']
         remetente = text_data_json['remetente']
 
+
+        await self.save_mensagem(remetente=remetente, mensagem=mensagem, grupo=self.grupo)
+
         await self.channel_layer.group_send(
             self.sala_grupo,
             {
@@ -43,3 +49,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'mensagem': mensagem,
             'remetente': remetente
         }))
+
+    @database_sync_to_async
+    def get_grupo(self, grupo_nome):
+        return Grupo.objects.get(nome=grupo_nome)
+    
+    @database_sync_to_async
+    def save_mensagem(self, remetente, mensagem, grupo):
+        nova_mensagem = Mensagem(nome_autor=remetente, conteudo=mensagem, grupo=grupo)
+        nova_mensagem.save()
